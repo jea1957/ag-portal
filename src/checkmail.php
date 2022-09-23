f17ec63912a0e874d046791f728f962006e3fb9a
Joergen Andreasen <jea@hvedevej22.dk>
2022-09-23 07:12:21 +0000

    Forgot to require smtp.php file

--- src/checkmail.php
+++ src/checkmail.php
@@ -1,9 +1,10 @@
 <?php
 
 // This is supposed to be called via an external cron job
-require_once __DIR__ . "/utils.php";
-require_once __DIR__ . "/lang_da.php";
-require_once __DIR__ . "/pdo.php";
+require_once __DIR__ . '/smtp.php';
+require_once __DIR__ . '/utils.php';
+require_once __DIR__ . '/lang_da.php';
+require_once __DIR__ . '/pdo.php';
 require_once __DIR__ . '/mails.php';
 
 $mails = new Mails($pdo);
